ca8d70fa15189224d1980ce4cbf0ef78dacde638
Severin Senn <50795012+SeverinSenn@users.noreply.github.com>
2022-01-16 17:25:29 +0100

    add Notification when near 20m of Note

--- app/src/main/java/com/example/mapnotes/Service/DataService.java
+++ app/src/main/java/com/example/mapnotes/Service/DataService.java
@@ -77,4 +77,5 @@ public class DataService extends Service {
             return DataService.this;
         }
     }
+
 }
--- app/src/main/java/com/example/mapnotes/Service/NotificationService.java
+++ app/src/main/java/com/example/mapnotes/Service/NotificationService.java
@@ -6,8 +6,10 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -15,6 +17,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,20 +29,29 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.example.mapnotes.MapsActivity;
 import com.example.mapnotes.R;
+import com.example.mapnotes.Util.EditTextTextWatcher;
+import com.example.mapnotes.ViewModel.EditNoteViewModel;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 public class NotificationService extends Service {
     public static final String CHANNEL_ID = "NotificationServiceForegroundServiceChannel";
     private android.location.LocationManager LocationManager;
+    private DataService DataService;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Intent intentDataService = new Intent(this, DataService.class);
+        bindService(intentDataService, connection, Context.BIND_AUTO_CREATE);
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return START_STICKY;
         }
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 420000, 50, locationListener);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 420000, 50, locationListener);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID,"Notification", NotificationManager.IMPORTANCE_DEFAULT);
@@ -52,17 +66,60 @@ public class NotificationService extends Service {
     private LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(@NonNull Location location) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setContentTitle("Location")
-                    .setContentText("Latitude:"+ location.getLatitude() + " | Longitude:" + location.getLongitude());
-            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getBaseContext());
-            managerCompat.notify(1,builder.build());
+            ArrayList<EditNoteViewModel> locations = DataService.getList();
+            for (EditNoteViewModel item : locations) {
+                LatLng latLngitem = item.getLatLng();
+                double dis = distance(latLngitem.latitude,location.getLatitude(),latLngitem.longitude,location.getLongitude(),0,0);
+                if(dis <= 20){
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext(), CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_launcher_background)
+                            .setContentTitle(item.getTitle())
+                            .setContentText(item.getBeschreibung());
+                    NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getBaseContext());
+
+                    managerCompat.notify(locations.indexOf(item),builder.build());
+                }
+            }
+
+
+
         }
     };
 
+    //distance in Meters
+    public static double distance(double lat1, double lat2, double lon1,
+                                  double lon2, double el1, double el2) {
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        double height = el1 - el2;
+
+        distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+        return Math.sqrt(distance);
+    }
+
+    public ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            DataService.LocalBinder binder = (DataService.LocalBinder) service;
+            DataService = binder.getService();
+        }
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {}
+    };
+
     @Override
-    public void onDestroy() { super.onDestroy();}
+    public void onDestroy() { super.onDestroy();
+        unbindService(connection);
+    }
     @Override
     public void onCreate() {
         super.onCreate();
